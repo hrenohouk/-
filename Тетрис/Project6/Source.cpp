d2d686040a3e78f4d5d63e80887fe3dbfb7f3290
hrenohouk <48544458+hrenohouk@users.noreply.github.com>
2019-06-11 19:36:21 +0300

    Add files via upload

--- Тетрис/Project6/Source.cpp
+++ Тетрис/Project6/Source.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
 #include <conio.h>
@@ -6,37 +5,38 @@
 #include <time.h>
 #include <string.h>
 using namespace std;
-void gotoxy(int xpos, int ypos); 
+void gotoxy(int xpos, int ypos);
 
-#define KEY_UP 72 //определение клавиши "вверх"
-#define KEY_DOWN 80 //определение клавиши "вниз"
-#define KEY_LEFT 75 //определение клавиши "влево"
-#define KEY_RIGHT 77 //определение клавиши "вправо"
-#define KEY_SPACE 32 //определение клавиши "пробел"
-#define KEY_ESC 27 //определение клавиши "escape"
-#define KEY_ENTER 13 //определение клавиши "enter"
+const int KEY_UP = 72;
+const int KEY_DOWN = 80;
+const int KEY_LEFT = 75;
+const int KEY_RIGHT = 77;
+const int KEY_SPACE = 32;
+const int KEY_ESC = 27;
+const int KEY_ENTER = 13;
 
-#define SIZEX 16//-ширина поля
-#define SIZEY 20//-длина поля
-#define FMAP_COUNTS 7 //количество фигур тетриса
+const int SIZEX = 16;
+const int SIZEY = 20;
+const int FMAP_COUNTS = 7;
 
 #define TRUE 1 
-#define FALSE 0  //логические переменные для создания фигур 
+#define FALSE 0   
 
-#define SCR_SP  '\42' //закрашивание поля
-#define SCR_OB  '\43' //закрашивание фигуры
+#define SCR_SP  '\42'  
+#define SCR_OB  '\43'  
 char main_color[] = "color ##";
-int screen[SIZEX][SIZEY] = { 0 }; //размер экрана
-int map[4][4]; //фигура
+int screen[SIZEX][SIZEY] = { 0 };
+int map[4][4];
 int px, py, score, nextmap;
-const char* GAME_TITLE ="**********  *********  **********  ********  **     ***   *********  \n"
-						"**  **  **  **         **  **  **  **    **  **    ****   **     **  \n"
-						"**  **  **  **         **  **  **  **    **  **   ** **   **         \n"
-						"    **      *********      **      ********  **  **  **   **         \n"
-						"    **      **             **      **        ** **   **   **         \n"
-						"    **      **             **      **        ****    **   **     **  \n"
-						"  ******    *********    ******   ****       ****    **   *********  \n\n";
-int fmap[FMAP_COUNTS][4][4] = //инициализация фигур тетриса
+int f = 4;
+const char* GAME_TITLE = "**********  *********  **********  ********  **     ***   *********  \n"
+"**  **  **  **         **  **  **  **    **  **    ****   **     **  \n"
+"**  **  **  **         **  **  **  **    **  **   ** **   **         \n"
+"    **      *********      **      ********  **  **  **   **         \n"
+"    **      **             **      **        ** **   **   **         \n"
+"    **      **             **      **        ****    **   **     **  \n"
+"  ******    *********    ******   ****       ****    **   *********  \n\n";
+int fmap[FMAP_COUNTS][4][4]
 {
 	{
 		{1, 1, 0, 0},
@@ -82,13 +82,17 @@ int fmap[FMAP_COUNTS][4][4] = //инициализация фигур тетриса
 	}
 };
 
-void print(void) 
+void print(void)
 {
 	int i, j;
 	int buff[SIZEX][SIZEY];
 
-	for (i = 0; i < SIZEY; i++) for (j = 0; j < SIZEX; j++) buff[j][i] = screen[j][i]; 
-	for (i = 0; i < 4; i++) for (j = 0; j < 4; j++) if (map[j][i]) buff[j + px][i + py] = 1; 
+	for (i = 0; i < SIZEY; i++)
+		for (j = 0; j < SIZEX; j++)
+			buff[j][i] = screen[j][i];
+	for (i = 0; i < f; i++)
+		for (j = 0; j < f; j++)
+			if (map[j][i]) buff[j + px][i + py] = 1;
 
 	gotoxy(0, 0);
 	for (i = 0; i < SIZEY; i++)
@@ -101,76 +105,78 @@ void print(void)
 	}
 
 	gotoxy(SIZEX + 1, 0);
-	printf("Колличество очков: %i", score); //количество очков, набираемое в ходе игры
+	cout << "Score:" << score;
 }
 
-void printnextmap(void) //напечатать следующую идущую фигуру в поле
+void printnextmap(void)
 {
 	int i, j;
 
 	gotoxy(SIZEX + 1, 2);
-	printf("Следующая: ");
+	cout << "Next: ";
 
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < f; i++)
 	{
 		gotoxy(SIZEX + 2, i + 3);
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < f; j++)
 		{
 			putchar(fmap[nextmap][j][i] == 0 ? ' ' : SCR_OB);
 		}
 	}
 }
- 
-int getkey(void)//пауза во время игры
+
+int getkey(void)
 {
 	int c;
 	if (_kbhit())
 	{
 		if ((c = _getch()) == 224)
 			c = _getch();
+
 		return c;
 	}
 	return FALSE;
 }
 
- void gotoxy(int xpos, int ypos)
+void gotoxy(int xpos, int ypos)
 {
 	COORD scrn;
 	HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
 	scrn.X = xpos; scrn.Y = ypos;
 	SetConsoleCursorPosition(hOuput, scrn);
 }
-  
-int valnewpos(int x, int y) 
+
+int valnewpos(int x, int y)
 {
 	int i, j;
 	if (x < 0) return FALSE;
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < f; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < f; j++)
 		{
 			if (map[j][i])
 			{
-				if ((j + x >= SIZEX) || (i + y >= SIZEY)) return FALSE;//проверка не зашла ли фигура за границы нашего экрана.
+				if ((j + x >= SIZEX) || (i + y >= SIZEY)) return FALSE;
 				if (screen[j + x][i + y])
 				{
-					return FALSE;//если да то вернем "фолс"
+					return FALSE;
 				}
 			}
 		}
 	}
-	return TRUE;//если нет фигура движется вниз.
+	return TRUE;
 }
 
 #define inv(x) ((x * (-1)) + 3)
- 
-void rotatemap(void) //поворот фигуры
+
+void rotatemap(void)
 {
+
 	int _map[4][4];
 	int i, j, sx = 4, sy = 4;
 
-	for (i = 0; i < 4; i++)
-		for (j = 0; j < 4; j++)
+	for (i = 0; i < f; i++)
+		for (j = 0; j < f; j++)
 		{
 			_map[j][i] = map[j][i];
 			if (map[j][i])
@@ -181,27 +187,28 @@ void rotatemap(void) //поворот фигуры
 			map[j][i] = 0;
 		}
 
-	for (i = 0; i < 4; i++)
-		for (j = 0; j < 4; j++)
-			if (_map[inv(i)][j]) map[j - sx][i - sy] = 1;//рисуем перевортыш
-	if (!valnewpos(px, py)) for (i = 0; i < 4; i++)
-		for (j = 0; j < 4; j++)
-			map[j][i] = _map[j][i];//возвращаем обратно на изначальную фигурку
+	for (i = 0; i < f; i++)
+		for (j = 0; j < f; j++)
+			if (_map[inv(i)][j]) map[j - sx][i - sy] = 1;
+	if (!valnewpos(px, py))
+		for (i = 0; i < f; i++)
+			for (j = 0; j < f; j++)
+				map[j][i] = _map[j][i];
 }
-  
-int rnd(int max) //рандомизация
+
+int rnd(int max)
 {
 	max++;
 	return (int)(rand() * max / RAND_MAX);
 }
-  
-void sleep(int milsec) //задержка
+
+void sleep(int milsec)
 {
 	clock_t t = clock();
 	while (clock() - t < milsec);
 }
-  
-void deleteline(void) //убить собранную линию
+
+void deleteline(void)
 {
 	int i, j, k, cl;
 
@@ -214,7 +221,6 @@ void deleteline(void) //убить собранную линию
 		}
 		if (cl)
 		{
-			//Анимация
 			gotoxy(0, i);
 			for (k = 0; k < SIZEX; k++) putchar('_'), sleep(20);
 
@@ -232,13 +238,13 @@ void deleteline(void) //убить собранную линию
 		}
 	}
 }
- 
-void createmap(void) //создание фигуры
+
+void createmap(void)
 {
 	int i, j;
 
-	for (i = 0; i < 4; i++)
-		for (j = 0; j < 4; j++)
+	for (i = 0; i < f; i++)
+		for (j = 0; j < f; j++)
 			map[j][i] = fmap[nextmap][j][i];
 	py = 0;
 	px = SIZEX / 2;
@@ -246,16 +252,16 @@ void createmap(void) //создание фигуры
 	nextmap = rnd(FMAP_COUNTS - 1);
 	printnextmap();
 }
- 
-void clearscreen(void) //очистка экрана
+
+void clearscreen(void)
 {
 	int i, j;
 	for (i = 0; i < SIZEY; i++)
 		for (j = 0; j < SIZEX; j++)
 			screen[j][i] = 0;
 }
- 
-void createrndscreen(void) //создать случайный экран
+
+void createrndscreen(void)
 {
 	int i, j, rn;
 	clearscreen();
@@ -266,21 +272,21 @@ void createrndscreen(void) //создать случайный экран
 			screen[j][i] = rnd(1);
 		}
 }
-  
-void startgame(void) //игра
+
+void startgame(void)
 {
 	int i, j, c;
 	time_t tm;
 
 	system("cls");
-	px = SIZEX / 2;//значение для падения фигуры по х,выход из центра
+	px = SIZEX / 2;
 	py = 0;
 	score = 0;
 
 	tm = clock();
 
-	nextmap = rnd(FMAP_COUNTS - 1);//следующая фигура выбирается из 6 
-	createmap();//создание фигурки
+	nextmap = rnd(FMAP_COUNTS - 1);
+	createmap();
 
 	while (TRUE)
 	{
@@ -289,82 +295,82 @@ void startgame(void) //игра
 		switch (c)
 		{
 		case KEY_UP:
-			rotatemap();//поворот
+			rotatemap();
 			break;
 		case KEY_SPACE:
-			rotatemap();//поворот
+			rotatemap();
 			break;
-		case KEY_DOWN://движение вниз
+		case KEY_DOWN:
 			for (; valnewpos(px, py + 1); py++);
-			for (i = 0; i < 4; i++)
-				for (j = 0; j < 4; j++)
+			for (i = 0; i < f; i++)
+				for (j = 0; j < f; j++)
 					if (map[j][i]) screen[px + j][py + i] = 1;
 
 			print();
-			deleteline();//проход по функции проверки заполнилась ли линия и её удаление
-			createmap();//новая фигура
+			deleteline();
+			createmap();
 			break;
 		case KEY_LEFT:
-			if (valnewpos(px - 1, py)) px--;//движение влево ,уменьшаем значение пера по х
+			if (valnewpos(px - 1, py)) px--;
 			break;
 		case KEY_RIGHT:
-			if (valnewpos(px + 1, py)) px++;//движение вправо ,увеличиваем значение пера по х
+			if (valnewpos(px + 1, py)) px++;
 			break;
-		case 'p'://пауза
+		case 'p':
 			_getch();
 			break;
-		case KEY_ESC://выход
+		case KEY_ESC:
 			return;
 		}
 
-		if ((clock() - tm) > 720)//время падения фигуры 
+		if ((clock() - tm) > 720)
 		{
 			tm = clock();
 
 			if (!(valnewpos(px, py + 1)))
 			{
-				for (i = 0; i < 4; i++)
-					for (j = 0; j < 4; j++)
+				for (i = 0; i < f; i++)
+					for (j = 0; j < f; j++)
 						if (map[j][i]) screen[px + j][py + i] = 1;
 
 				createmap();
 				deleteline();
 			}
-			else py++;//падение вниз
+			else py++;
 		}
 
 		print();
 
 		for (i = 0; i < SIZEX; i++)
 		{
-			if (screen[i][0])//если экраный у и у фигуры совпали и равны 0,то конец игры ,поле заполнилось до верха
+			if (screen[i][0])
 			{
 				system("cls");
 				gotoxy(2, 8);
-				printf("GAME OVER!!1!");
+				cout << "GAME OVER!!1!";
 				return;
 			}
 		}
 	}
 }
- 
-void gamemenu(void) //игровое меню
+
+void gamemenu(void)
 {
 	int p = 1, c = 0;
 	const char* GAME_MENU = "                     +===============================+\n"
-		"                     |  1. Старт                     |\n"
+		"                     |  1. Start                     |\n"
 		"                     +===============================+\n"
-		"                     |  2. Старт(ПОВЫШ.УРОВЕНЬ)      |\n"
+		"                     |  2. Start(difficult)          |\n"
 		"                     +===============================+\n"
-		"                     |  3. УПРАВЛЕНИЕ                |\n"
+		"                     |  3. Control                   |\n"
 		"                     +===============================+\n"
-		"                     |  4. ВЫХОД                     |\n "
+		"                     |  4. Exit the game             |\n "
 		"                    +===============================+";
-	system("cls"); printf("%s%s", GAME_TITLE, GAME_MENU);
-	while (TRUE) //навигация по меню
+	system("cls");
+	cout << GAME_TITLE << GAME_MENU;
+	while (TRUE)
 	{
 		c = _getch();
-		printf("\a");
 		switch (c)
 		{
 		case '1':
@@ -378,34 +384,38 @@ void gamemenu(void) //игровое меню
 			case 1:
 				clearscreen();
 				startgame();
-				gotoxy(0, SIZEY); printf("Нажми ESC для выхода в главное меню...\n"); printf("\a"); while (_getch() != KEY_ESC);
+				gotoxy(0, SIZEY);
+				cout << "Press ESC to exit to the main menu...";
+				while (_getch() != KEY_ESC);
 				break;
 			case 2:
 				clearscreen();
 				createrndscreen();
 				startgame();
-				gotoxy(0, SIZEY); printf("Нажми ESC для выхода в главное меню...\n"); printf("\a"); while (_getch() != KEY_ESC);
+				gotoxy(0, SIZEY);
+				cout << "Press ESC to exit to the main menu...";
+				while (_getch() != KEY_ESC);
 				break;
 			case 3:
 				system("cls");
-				printf("%s%s", GAME_TITLE,
-					"+-------+-------+\n"
-					"|<      |Влево  |\n"
-					"|>      |Вправо |\n"
-					"|       |Вниз   |\n"
-					"|Space  |Поворот|\n"
-					"|P      |Пауза  |\n"
-					"|ESC    |Выход  |\n"
-					"+-------+-------+\n\n"
-					"Нажми клавишу...\n");
+				cout << GAME_TITLE <<
+					"+=======+=======+\n"
+					"|<      |left   |\n"
+					"|>      |right  |\n"
+					"|       |down   |\n"
+					"|Space  |Turn   |\n"
+					"|P      |Pause  |\n"
+					"|ESC    |exit   |\n"
+					"+=======+=======+\n\n"
+					"Press the key...\n";
 				_getch();
-				printf("\a");
 				if (_kbhit())
 					_getch();
 				break;
 			case 4: return;
 			}
-			system("cls"); printf("%s%s", GAME_TITLE, GAME_MENU);
+			system("cls");
+			cout << GAME_TITLE << GAME_MENU;
 			p = 1;
 			break;
 		case KEY_UP:
@@ -420,16 +430,14 @@ void gamemenu(void) //игровое меню
 
 	}
 }
- 
+
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 	system("color 47");
-	printf("%s", GAME_TITLE);
-	printf("              Нажмите любую клавишу для продолжение");
+	cout << GAME_TITLE;
+	cout << "              Press any key to continue.";
 
 	_getch();
-	printf("\a");
 	if (_kbhit()) _getch();
 	gamemenu();
 }
